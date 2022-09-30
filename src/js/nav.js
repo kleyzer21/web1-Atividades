35f951a2feba4036af51cb05bf5fc874e9afba87
kleyzer21 <edimar.silva2@estudante.ifms.edu.br>
2022-09-29 23:47:22 -0300

    Aula29/09

---
+++ src/js/nav.js
@@ -0,0 +1,21 @@
+export const nav = /*html */ `
+<nav class="container-fluid">
+<ul>
+  <li><a class="contrast" href=" ./index.html">Desenvolvimento Web 1</a></li>
+</ul>
+<!-- Menu Principal-->
+<ul>
+  <li>
+    <details role="list" dir="rtl">
+      <summary role="link" class="contrast">Exercícios</summary> <!-- Texto menu Principal-->
+      <ul role="listbox">
+        <!-- Sub Menu-->
+        <li><a class="primary" href="./login.html">Formulário de Acesso</a></li> <!-- Item do submenu-->
+      </ul>
+      <!-- fim do Sub Menu-->
+    </details>
+  </li>
+</ul>
+<!-- Fim do Menu Principal-->
+</nav>
+`
